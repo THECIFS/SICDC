1b913d35fb6ddbe2ef3c195c3fcaef429b8f4edc
Carlos Ivan Flores Sanchez <crascifs@gmail.com>
2026-05-19 10:23:12 -0600

    Cambios en modelos - Repositorio / Creacion de Controladores (Administrador - Area)

--- repository/administradorRepository.js
+++ repository/administradorRepository.js
@@ -1,5 +1,5 @@
 import { pool } from '../config/postgres'
-import Administrador from '../model/administradorModel'
+import {Administrador} from '../model/administradorModel'
 
 const queryCreate = `
     INSERT INTO administradores (
